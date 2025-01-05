f8c78e513d1c92b1dc309025aa26208d9957f7aa
qinxuanlong <1470681411@qq.com>
2025-01-05 14:02:36 +0800

    修改相对路径

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
+  build:{
+    outDir: 'docs', //指定输出路径
+  },
   plugins: [vue()],
 })
