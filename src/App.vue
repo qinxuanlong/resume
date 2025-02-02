d63c9da4cf277b947c4e4ac7824fd740ba6ba151
qinxuanlong <1470681411@qq.com>
2025-02-02 20:49:17 +0800

    页面修改

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import meInfo from './components/meInfo.vue'
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  </div> -->
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+   <meInfo/>
+  <RouterLink to="/" >Home</RouterLink>  
+  <b>/</b>
+  <RouterLink to="/about">About</RouterLink>
+  <RouterView>
+    <template #default="{ Component, route }">
+      <Transition name="fade" mode="out-in">
+        <component :is="Component" :key="route.fullPath" />
+      </Transition>
+    </template>
+  </RouterView>
 </template>
 
 <style scoped>
---
+++ src/router/index.ts
@@ -0,0 +1,17 @@
+import { createMemoryHistory, createRouter } from 'vue-router'
+
+
+import AboutView from '../view/AboutView.vue'
+import HomeView from '../view/HomView.vue'
+
+const routes = [
+  { path: '/', component: HomeView },
+  { path: '/about', component: AboutView },
+]
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+})
+
+export default router
